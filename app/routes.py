e9abda7bfeef75af9a90e5d46337c851fab6c2a3
Pedro <pedro8correa@gmail.com>
2019-01-20 16:04:29 -0200

    Added: Verificação do tipo de request na rota de lanche
    Foi adicionado a verificação do método get e post, pois esta rota irá receber os novos ingredientes extras que o usuário for adicionar no lanche.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from app import app, functions
 import json
 import pprint as pp
@@ -8,14 +8,23 @@ import pprint as pp
 def index():
   return render_template('index.html', title='Lanches', lanches=functions.get_all_together())
 
-@app.route('/lanche/<codigo>', methods=['GET'])
+@app.route('/lanche/<codigo>', methods=['GET', 'POST'])
 def lanche(codigo):
   lanche = functions.filtra_lanche(int(codigo))
-  preco = functions.calcula_preco(lanche)
-  lanche = functions.get_ingredientes_lanche(lanche)
-  return render_template('lanche.html', title='Lanche escolhido', 
-          lanche=lanche, ingredientes=functions.get_all_ingredientes(),
-          preco=preco)
+  if request.method == 'GET':
+    preco = functions.calcula_preco(lanche)
+    lanche = functions.get_ingredientes_lanche(lanche)
+    return render_template('lanche.html', title='Lanche escolhido', 
+            lanche=lanche, ingredientes=functions.get_all_ingredientes(),
+            preco=preco)
+  else:
+    data = request.get_json()
+    if len(data) > 0:
+      for extra in data:
+        lanche['ingredientes'].append(int(extra))
+    preco = functions.calcula_preco(lanche)
+    lanche = functions.get_ingredientes_lanche(lanche)
+    return jsonify({ "lanche": lanche, "preco": preco })
 
 
 @app.route('/<codigo>', methods=['GET'])
