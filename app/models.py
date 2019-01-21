6db79e3a16314b2ee7bac87702309f85a2c9ca06
Pedro <pedro8correa@gmail.com>
2019-01-21 07:37:43 -0200

    Added: Models de Lanche
    Foi criado o models de lanche para ser utilizado.
    No front do index foi arrumado ao setar o id no botao para receber o id do model Lanche.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 class Ingrediente():
   def __init__(self, nome, preco):
     self.nome = nome
@@ -5,3 +7,23 @@ class Ingrediente():
   
   def __repr__(self):
     return '<Ingrediente {}>'.format(self.nome)
+
+
+class Lanche():
+  def __init__(self, nome, ingredientes):
+    self._id = -1
+    self.nome = nome
+    self.ingredientes = ingredientes
+
+  def __repr__(self):
+    return '<Lanche {}>'.format(self.nome)
+
+  def setId(self, codigo):
+    self._id = codigo
+
+  def toJSON(self):
+    return jsonify({
+            "id": self._id,
+            "nome": self.nome,
+            "ingredientes": self.ingredientes
+          })
