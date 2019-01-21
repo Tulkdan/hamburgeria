307ae846ba319aeeaadcf2d6a636b533474a14fa
Pedro <pedro8correa@gmail.com>
2019-01-21 08:32:10 -0200

    Fix: Sessão e rota de montar
    Foi adicionado a sessão que irá guardar a escolha final do lanche, sendo ele personalizado ou montado desde o início, a sessão será utilizada para a rota de finalizar mostrar os ingredientes finais.
    Arrumado na rota de montar para que os ingredientes escolhidos seja passados para a página finalizar.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, jsonify, request, redirect, url_for
-from app import app, functions
+from flask import render_template, jsonify, request, redirect, url_for, session
+from app import app, functions, models
 import json
 import pprint as pp
 
@@ -21,24 +21,27 @@ def lanche(codigo):
     data = request.get_json()
     if len(data) > 0:
       for extra in data:
-        lanche['ingredientes'].append(int(extra))
+        lanche.ingredientes.append(int(extra))
     preco = functions.calcula_preco(lanche)
     lanche = functions.get_ingredientes_lanche(lanche)
-    return redirect(url_for('finalizar', lanche=lanche, preco=preco))
+    session['lanche'] = lanche.toJSON()
+    session['preco'] = preco
+    return redirect(url_for('finalizar'))
 
+'''
 @app.route('/<codigo>', methods=['GET'])
 def ingrediente(codigo):
   lanche = functions.filtra_lanche(int(codigo))
   preco = functions.calcula_preco(lanche)
   return jsonify({ "lanche": lanche, "preco": preco })
+'''
 
 @app.route('/finalizar', methods=['GET'])
 def finalizar():
-  lanche = request.args.get('lanche')
-  preco = request.args['preco']
-  json_acceptable = lanche.replace("'", "\"")
-  lanche = json.loads(json_acceptable)
-  return render_template('finalizado.html', title=lanche['nome'],
+  lanche = session.get('lanche', None)
+  preco = session.get('preco', None)
+  lanche = models.Lanche(lanche['nome'], lanche['ingredientes'])
+  return render_template('finalizado.html', title=lanche.nome,
           lanche=lanche, preco=preco)
 
 @app.route('/montar', methods=['GET', 'POST'])
@@ -46,3 +49,12 @@ def montar():
   if request.method == 'GET':
     ingredientes = functions.get_all_ingredientes()
     return render_template('montar.html', title='Montar lanche', ingredientes=ingredientes)
+  else:
+    data = request.get_json()
+    ingredientes = list(map(lambda x: int(x), data))
+    lanche = models.Lanche('', ingredientes)
+    preco = functions.calcula_preco(lanche)
+    lanche = functions.get_ingredientes_lanche(lanche)
+    session['lanche'] = lanche.toJSON()
+    session['preco'] = preco
+    return redirect(url_for('finalizar'))
