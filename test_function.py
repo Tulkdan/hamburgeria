8da637a47da5772f8069462bf390f47bdd962c45
Pedro <pedro8correa@gmail.com>
2019-01-21 08:45:36 -0200

    Added: name main to test_function file

--- test_function.py
+++ test_function.py
@@ -1,5 +1,5 @@
 import unittest
-from app.functions import *
+from app.functions import calcula_preco, filtra_lanche, get_all_ingredientes, get_all_together, get_ingredientes_lanche
 from app.models import Ingrediente, Lanche
 
 class Testes(unittest.TestCase):
@@ -21,5 +21,6 @@ class Testes(unittest.TestCase):
   def test_get_ingredientes(self):
     lanche = filtra_lanche(1)
     self.assertEqual(len(get_ingredientes_lanche(lanche).ingredientes), 2)
-  
-unittest.main()
+
+if __name__ == '__main__':
+  unittest.main(verbosity=2)
