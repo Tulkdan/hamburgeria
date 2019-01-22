54d22fd9614461a63cd635b0fe18a532f6aedf83
Pedro <pedro8correa@gmail.com>
2019-01-22 21:16:49 -0200

    Fix: Verificações de itens antes de renderizar páginas
    Foi adicionado verificações que para a aplicação não quebrar ao não encontrar itens com X id ou com valores vazios.

--- app/routes.py
+++ app/routes.py
@@ -11,13 +11,13 @@ def index():
 @app.route('/lanche/<codigo>', methods=['GET', 'POST'])
 def lanche(codigo):
   lanche = functions.filtra_lanche(int(codigo))
-  if request.method == 'GET':
+  if request.method == 'GET' and lanche:
     lanche = functions.get_ingredientes_lanche(lanche)
     preco = functions.calcula_preco(lanche)
     return render_template('lanche.html', title='Lanche escolhido', 
             lanche=lanche, ingredientes=functions.get_all_ingredientes(),
             preco=preco)
-  else:
+  elif lanche:
     data = request.get_json()
     if len(data) > 0:
       for extra in data:
@@ -27,32 +27,29 @@ def lanche(codigo):
     session['lanche'] = lanche.toJSON()
     session['preco'] = preco
     return redirect(url_for('finalizar'))
-
-'''
-@app.route('/<codigo>', methods=['GET'])
-def ingrediente(codigo):
-  lanche = functions.filtra_lanche(int(codigo))
-  preco = functions.calcula_preco(lanche)
-  return jsonify({ "lanche": lanche, "preco": preco })
-'''
+  else:
+    return redirect(url_for('index'))
 
 @app.route('/finalizar', methods=['GET'])
 def finalizar():
   aux = session.get('lanche', None)
   preco = session.get('preco', None)
-  lanche = models.Lanche(aux['nome'], [])
-  for ingrediente in aux['ingredientes']:
-    lanche.add_ingrediente(models.Ingrediente(ingrediente['id'], ingrediente['nome'], ingrediente['preco']))
-  promos = functions.Promos()
-  desconto = 0
-  desconto += promos.light(lanche, preco)
-  desconto += promos.too_much(lanche, 'Hambúrguer de carne')
-  desconto += promos.too_much(lanche, 'Queijo')
-  desconto = round(desconto, 2)
-  if desconto > 0:
-    preco = preco - desconto
-  return render_template('finalizado.html', title=lanche.nome,
-          lanche=lanche, preco=preco, desconto=desconto)
+  if aux and preco:
+    lanche = models.Lanche(aux['nome'], [])
+    for ingrediente in aux['ingredientes']:
+      lanche.add_ingrediente(models.Ingrediente(ingrediente['id'], ingrediente['nome'], ingrediente['preco']))
+    promos = functions.Promos()
+    desconto = 0
+    desconto += promos.light(lanche, preco)
+    desconto += promos.too_much(lanche, 'Hambúrguer de carne')
+    desconto += promos.too_much(lanche, 'Queijo')
+    desconto = round(desconto, 2)
+    if desconto > 0:
+      preco = preco - desconto
+    return render_template('finalizado.html', title=lanche.nome,
+            lanche=lanche, preco=preco, desconto=desconto)
+  else:
+    return redirect(url_for('index'))
 
 @app.route('/montar', methods=['GET', 'POST'])
 def montar():
